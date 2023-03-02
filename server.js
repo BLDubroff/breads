0c584cdc4bc338fada682cf6eeeea336868de772
Ben Dubroff <BLDubroff@gmail.com>
2023-03-01 19:28:28 -0500

    MongoDB and mongoose additions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -15,6 +16,16 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }).then(() => {
+    console.log('connected to mongo: ', process.env.MONGO_URI);
+}).catch((err) => {
+    console.error('Error connecting to MongoDB: ', err)
+});
+  
 
 // ROUTES
 app.get('/', (req, res) => {
