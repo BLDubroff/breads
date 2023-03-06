ee8e1ad976060419ceadddc0810f45a84d68f5e6
Ben Dubroff <BLDubroff@gmail.com>
2023-03-06 18:05:49 -0500

    schema validation activity

--- views/show.jsx
+++ views/show.jsx
@@ -17,6 +17,7 @@ function Show ({bread, index}) {
         </p>
         <img src={bread.image} alt={bread.name} />
         <li><a href="/breads">Go home</a></li>
+        <p>Baked by {bread.baker}</p>
         {/* Delete button */}
         <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type='submit' value="DELETE"/>
